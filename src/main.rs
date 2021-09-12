9a1f3b2c1f3d3d457d316148b86f4a4383ee9eb4
Alexander Montgomery <alexandermontgomery95@gmail.com>
2021-09-12 16:20:07 -0400

    Adding healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use warp::Filter;
+use warp::http::StatusCode;
 
 #[tokio::main]
 async fn main() {
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
-    warp::serve(hello)
+    let health = warp::path!("health")
+        .map( || StatusCode::OK);
+
+    let routes = health.or(hello)
+        .with(warp::cors().allow_any_origin());
+
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
         .await;
 }
